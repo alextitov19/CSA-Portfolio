f2a456b3b97ea0136d650f44ea99dbf915465a15
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2020-10-13 16:13:06 -0700

    added more buttons

--- src/CalculatorUI/CalculatorUI.java
+++ src/CalculatorUI/CalculatorUI.java
@@ -13,4 +13,12 @@ public class CalculatorUI {
     private JButton button3;
     private JButton button6;
     private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JButton button12;
+    private JButton button13;
+    private JButton button14;
+    private JButton button15;
+    private JButton button16;
+    private JButton button17;
 }
