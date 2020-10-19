44be9fe99e93267ebd3f9373d3c6c9c6502fa4e2
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2020-10-19 12:41:42 -0700

    Able to plot, no graph yet

---
+++ src/GraphingCalculator/GraphingCalculator.java
@@ -0,0 +1,32 @@
+package GraphingCalculator;
+
+import java.awt.Component;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+
+public class GraphingCalculator {
+
+    private int[] xs = {20, 40, 60, 80, 100};
+
+
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.add(new CustomPaintComponent());
+
+        int frameWidth = 300;
+
+        int frameHeight = 300;
+
+        frame.setSize(frameWidth, frameHeight);
+
+        frame.setVisible(true);
+
+    }
+
+    public int[] GetCoords() {
+        return xs;
+    }
+
+}
