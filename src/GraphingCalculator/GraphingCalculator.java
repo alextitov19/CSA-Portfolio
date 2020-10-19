041d07bb4fbde4db90f496d3d67e81f22be02efc
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2020-10-19 13:38:09 -0700

    able to graph

--- src/GraphingCalculator/GraphingCalculator.java
+++ src/GraphingCalculator/GraphingCalculator.java
@@ -4,36 +4,47 @@ import java.awt.*;
 
 public class GraphingCalculator {
 
-    private int numberOfPoints = 20;
+    private int numberOfPoints = 400;
 
     private int[][] coordinates = new int[2][numberOfPoints];
 
 
 
     public static void main(String[] args) {
+
+
+
+
         Frame frame = new Frame();
         frame.add(new CustomPaintComponent());
 
-        int frameWidth = 300;
+        int frameWidth = 1000;
 
-        int frameHeight = 300;
+        int frameHeight = 1000;
 
         frame.setSize(frameWidth, frameHeight);
 
         frame.setVisible(true);
 
+
+
     }
 
     //takes in a form of y=mx+b
-    public void CalculateLinear(int m, int b) {
+    private int[][] CalculateLinear(int m, int b) {
+        int[][] arr = new int[2][numberOfPoints];
         for (int i = 0; i < numberOfPoints; i++) {
-            coordinates[0][i] = i;
-            coordinates[1][i] = (m * i) + b;
+            arr[0][i] = i;
+            arr[1][i] = (m * i) + b;
+            System.out.printf("i:%d, X: %d, Y:%d\n", i, coordinates[0][i], coordinates[1][i]);
         }
+        return arr;
     }
 
     public int[][] GetCoords() {
-        return coordinates;
+        int[][] arr = {{10, 20, 30, 40}, {10, 20, 30, 40}};
+        GraphingCalculator gc = new GraphingCalculator();
+        return  gc.CalculateLinear(1, 7);
     }
 
 }
