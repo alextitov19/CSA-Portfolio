2c4f1616c7e42f5d833f7242bd54fefa2e526674
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2020-10-19 23:02:46 -0700

    graphinc calc works

--- src/GraphingCalculator/GraphingCalculator.java
+++ src/GraphingCalculator/GraphingCalculator.java
@@ -6,30 +6,36 @@ public class GraphingCalculator {
 
     private int numberOfPoints = 400;
 
+    public double m, a, n, b;
+
+    public double[][] coords;
+
     public static void main(String[] args) {
-        Frame frame = new Frame();
-        frame.add(new CustomPaintComponent());
-        int frameWidth = 1000;
-        int frameHeight = 1000;
-        frame.setSize(frameWidth, frameHeight);
-        frame.setVisible(true);
+
     }
 
     //takes in a form of y=m((x+a)^n)+b
-    private double[][] CalculateLinear(int m, int a, int n, int b) {
+    public void CalculateLinear() {
         double[][] arr = new double[2][numberOfPoints];
         for (int i = 0; i < numberOfPoints; i++) {
             arr[0][i] = i;
             arr[1][i] = (m * Math.pow((i + a), n)) + b;
             System.out.println(arr[1][i]);
         }
-        return arr;
+        coords = arr;
     }
 
-    public double[][] GetCoords() {
-        double[][] arr = {{10, 20, 30, 40}, {10, 20, 30, 40}};
-        GraphingCalculator gc = new GraphingCalculator();
-        return  gc.CalculateLinear(1, 0, 2, 0);
+    public void SetVars(double[] arr) {
+
+    }
+
+    public void RunGraphingCalculator() {
+        Frame frame = new Frame();
+        frame.add(new CustomPaintComponent());
+        int frameWidth = 1000;
+        int frameHeight = 1000;
+        frame.setSize(frameWidth, frameHeight);
+        frame.setVisible(true);
     }
 
 }
