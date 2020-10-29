1ae34e12aff8b109fe3348e4ea4e507bbb891b38
banditcat12 <banditcat12@gmail.com>
2020-10-28 23:09:07 -0700

    Update Quiz.java

--- src/Quiz/Quiz.java
+++ src/Quiz/Quiz.java
@@ -1,7 +1,7 @@
 package Quiz;
 
 public class Quiz {
-    public static void main(String[] args){
+    public static void main(String[] args){ //just test code, as GUI has not yet been programed
         QuestionGenerator currentQuestion = new QuestionGenerator(2,9,10);
         System.out.println(currentQuestion.getQuestionText());
         for(int i=0; i<4; i++) {
