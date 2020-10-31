9cec16948072bb367011eb6627b22c8dc0295ded
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2020-10-30 23:53:41 -0700

    reads xml file

--- src/Playground/XmlReader.java
+++ src/Playground/XmlReader.java
@@ -1,19 +1,26 @@
 package Playground;
 
 import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
 import javax.xml.parsers.*;
 import java.io.*;
 
 public class XmlReader {
 
-    public static void main(String[] args) throws ParserConfigurationException {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
 
-        //Document document = builder.parse(new File("Users.xml"));
+        //get path of repo root and concatinate to get the path of xml file
         String path = System.getProperty("user.dir");
-        System.out.println();
+        System.out.println(path);
+        path += "/XMLs/Users";
+
+        //create xml doc
+        Document document = builder.parse(new File(path));
+        System.out.println("Doc loaded");
 
     }
 }
