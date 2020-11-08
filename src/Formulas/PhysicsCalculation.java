59fc5549191bac68e4d6b0af6f54fb7e5fb0020b
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2020-11-08 10:29:07 -0800

    added physics formulas model

--- src/Formulas/PhysicsCalculation.java
+++ src/Formulas/PhysicsCalculation.java
@@ -13,15 +13,25 @@ public class PhysicsCalculation {
         t = values[5];
     }
 
-    public double Calculate(int op) {
+    public double Calculate(int op) throws Exception {
+        double answer;
         switch (op){
             case 1:
+                answer = vi + (a*t);
+                break;
             case 2:
+                answer = xi + (vi * t) + (0.5 * a * t * t);
+                break;
             case 3:
+                answer = Math.sqrt((vi * vi) + (2 * a * (xf - xi)));
+                break;
             case 4:
+                answer = xi + (0.5 * (vf + vi) * t);
+                break;
             default:
+                throw new Exception("PhysicsCalculation.Calculate.op value not 1-4");
         }
-        return 0;
+        return answer;
     }
 
 }
