659c2497bc731f6d1601e1c7cd6919f53092189d
andrewzpu <andrewzpu@gmail.com>
2020-11-18 19:28:39 -0800

    Updated Higher Lower MVC Code

--- src/Games/MVCHighLowMain.java
+++ src/Games/MVCHighLowMain.java
@@ -1,5 +1,8 @@
 package Games;
 
+import javax.swing.*;
+import javax.swing.text.View;
+
 public class MVCHighLowMain {
 
     public static void main(String[] args) {
@@ -7,7 +10,14 @@ public class MVCHighLowMain {
     }
 
     public static void RunHL() {
-        HLControl.RunControl();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                HLModel model = new HLModel();
+                HLView view = new HLView();
+                HLControl controller = new HLControl(model,view);
+            }
+        });
     }
 
 }
