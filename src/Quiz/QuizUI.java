92422765e22500ac49f562ea1c9abb7c9bc4dc67
banditcat12 <banditcat12@gmail.com>
2020-11-19 05:43:05 -0800

    Finished Quiz
    Signed-off-by: banditcat12 <banditcat12@gmail.com>

--- src/Quiz/QuizUI.java
+++ src/Quiz/QuizUI.java
@@ -1,11 +1,11 @@
 package Quiz;
 
 import Playground.FileReader;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class QuizUI {
 
@@ -14,24 +14,29 @@ public class QuizUI {
     private JButton button2;
     private JButton button3;
     private JButton button4;
+    private JLabel evalLabel;
+    private JLabel ansLabel;
     private JLabel questionLabel;
 
     private QuestionGenerator currentQuestion = new QuestionGenerator(1, 1, 1);
     private int currentAnswer;
+    private int coinsEarnedInSession = 0;
 
     public static void main(String[] args) {
         RunQuizUI();
     }
 
     public QuizUI() {
-        button1.setText("");
-        button2.setText("");
-        button3.setText("");
-        button4.setText("");
+        button1.setText("A");
+        button2.setText("B");
+        button3.setText("C");
+        button4.setText("D");
+        evalLabel.setText("Coins Earned: ");
         questionLabel.setText("Press any button to start");
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                ansLabel.setText("Previous Answer:!".replace("!",Integer.toString(currentAnswer)));
                 if (button1.getText().equals(String.valueOf(currentAnswer))) {
                     System.out.println("Correct!");
                     FileReader fileReader = new FileReader();
@@ -39,8 +44,13 @@ public class QuizUI {
                     int coins = atts[0];
                     int xp = atts[1];
                     coins += 5;
-                    atts = new int[]{coins, xp};
-                    fileReader.SetValue(atts);                } else {
+                    fileReader.SetValue(atts);
+                    System.out.println(fileReader.GetAttributes().toString());
+                    coinsEarnedInSession+=5;
+                    evalLabel.setText("Coins Earned:!".replace("!",String.valueOf(coinsEarnedInSession)));
+                }
+
+                else if (!button1.getText().equals("A")){
                     System.out.println("Incorrect");
                 }
 
@@ -51,6 +61,7 @@ public class QuizUI {
         button2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                ansLabel.setText("Previous Answer:!".replace("!",Integer.toString(currentAnswer)));
                 if (button2.getText().equals(String.valueOf(currentAnswer))) {
                     System.out.println("Correct!");
                     FileReader fileReader = new FileReader();
@@ -58,8 +69,12 @@ public class QuizUI {
                     int coins = atts[0];
                     int xp = atts[1];
                     coins += 5;
-                    atts = new int[]{coins, xp};
-                    fileReader.SetValue(atts);                } else {
+                    FileReader filereader = new FileReader();
+                    fileReader.SetValue(atts);
+                    coinsEarnedInSession+=5;
+                    evalLabel.setText("Coins Earned:!".replace("!",String.valueOf(coinsEarnedInSession)));
+                }
+                else if (!button1.getText().equals("B")){
                     System.out.println("Incorrect");
                 }
 
@@ -70,6 +85,7 @@ public class QuizUI {
         button3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                ansLabel.setText("Previous Answer:!".replace("!",Integer.toString(currentAnswer)));
                 if (button3.getText().equals(String.valueOf(currentAnswer))) {
                     System.out.println("Correct!");
                     FileReader fileReader = new FileReader();
@@ -77,8 +93,11 @@ public class QuizUI {
                     int coins = atts[0];
                     int xp = atts[1];
                     coins += 5;
-                    atts = new int[]{coins, xp};
-                    fileReader.SetValue(atts);                } else {
+                    FileReader filereader = new FileReader();
+                    fileReader.SetValue(atts);
+                    coinsEarnedInSession+=5;
+                    evalLabel.setText("Coins Earned:!".replace("!",String.valueOf(coinsEarnedInSession)));}
+                else if (!button1.getText().equals("C")){
                     System.out.println("Incorrect");
                 }
 
@@ -89,6 +108,7 @@ public class QuizUI {
         button4.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                ansLabel.setText("Previous Answer:!".replace("!",Integer.toString(currentAnswer)));
                 if (button4.getText().equals(String.valueOf(currentAnswer))) {
                     System.out.println("Correct!");
                     FileReader fileReader = new FileReader();
@@ -96,9 +116,12 @@ public class QuizUI {
                     int coins = atts[0];
                     int xp = atts[1];
                     coins += 5;
-                    atts = new int[]{coins, xp};
+                    FileReader filereader = new FileReader();
                     fileReader.SetValue(atts);
-                } else {
+                    coinsEarnedInSession+=5;
+                    evalLabel.setText("Coins Earned:!".replace("!",String.valueOf(coinsEarnedInSession)));
+                }
+                else if (!button1.getText().equals("D")){
                     System.out.println("Incorrect");
                 }
 
@@ -111,7 +134,7 @@ public class QuizUI {
         JFrame frame = new JFrame("QuizUI");
         frame.setContentPane(new QuizUI().panel1);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.pack();
+        frame.setSize(1000,500);
         frame.setVisible(true);
     }
 
@@ -123,7 +146,7 @@ public class QuizUI {
     }
 
     public void GetNewQuestion() { //must be randomized
-        currentQuestion.questionType = (int) (Math.random() * ((2 - 1) + 1)) + 1;
+        currentQuestion.questionType = (int) (Math.random() * ((5 - 1) + 1)) + 1;
         currentQuestion.param1 = (int) (Math.random() * ((999 - 0) + 1)) + 0;
         currentQuestion.param2 = (int) (Math.random() * ((999 - 0) + 1)) + 0;
 
@@ -133,11 +156,12 @@ public class QuizUI {
         questionLabel.setText(c.getQuestionText());
     }
 
-    public void displayChoices(QuestionGenerator c) { // adds possible choices to buttons, needs to be randomized later
-        button1.setText(String.valueOf(currentAnswer));
-        button2.setText("0");
-        button3.setText("0");
-        button4.setText("0");
+    public void displayChoices(QuestionGenerator c) { // adds possible choices to buttons
+        ArrayList l = currentQuestion.getPossibleAnswers();
+        button1.setText(String.valueOf(l.get(0)));
+        button2.setText(String.valueOf(l.get(1)));
+        button3.setText(String.valueOf(l.get(2)));
+        button4.setText(String.valueOf(l.get(3)));
     }
 
 
@@ -162,54 +186,74 @@ public class QuizUI {
         questionLabel.setText("Label");
         GridBagConstraints gbc;
         gbc = new GridBagConstraints();
-        gbc.gridx = 2;
-        gbc.gridy = 0;
-        gbc.gridheight = 4;
-        gbc.weighty = 1.0;
-        gbc.anchor = GridBagConstraints.WEST;
-        panel1.add(questionLabel, gbc);
-        button1 = new JButton();
-        button1.setText("");
-        gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.weightx = 1.0;
+        gbc.gridwidth = 4;
         gbc.weighty = 1.0;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel1.add(button1, gbc);
-        button2 = new JButton();
-        button2.setText("");
+        gbc.fill = GridBagConstraints.NONE;
+        panel1.add(questionLabel, gbc);
+
+        evalLabel = new JLabel();
+        evalLabel.setText("");
         gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.weightx = 1.0;
         gbc.weighty = 1.0;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.fill = GridBagConstraints.BOTH;
+        panel1.add(evalLabel, gbc);
+
+        ansLabel = new JLabel();
+        ansLabel.setText("");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.BOTH;
+        panel1.add(ansLabel, gbc);
+
+        button1 = new JButton();
+        button1.setText("");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.BOTH;
+        panel1.add(button1, gbc);
+        button2 = new JButton();
+        button2.setText("");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.BOTH;
         panel1.add(button2, gbc);
         button3 = new JButton();
         button3.setText("");
         gbc = new GridBagConstraints();
-        gbc.gridx = 0;
+        gbc.gridx = 2;
         gbc.gridy = 2;
         gbc.weightx = 1.0;
         gbc.weighty = 1.0;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.fill = GridBagConstraints.BOTH;
         panel1.add(button3, gbc);
         button4 = new JButton();
         button4.setText("");
         gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 3;
+        gbc.gridx = 3;
+        gbc.gridy = 2;
         gbc.weightx = 1.0;
         gbc.weighty = 1.0;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.fill = GridBagConstraints.BOTH;
         panel1.add(button4, gbc);
         final JPanel spacer1 = new JPanel();
         gbc = new GridBagConstraints();
-        gbc.gridx = 1;
+        gbc.gridx = 4;
         gbc.gridy = 2;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        panel1.add(spacer1, gbc);
+        gbc.fill = GridBagConstraints.BOTH;
     }
 
     /**
