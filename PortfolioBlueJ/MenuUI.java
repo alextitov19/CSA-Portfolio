1a289a2a47f953f2ad120435c5a9a87636ae313e
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2020-11-22 17:32:21 -0800

    added bluej

---
+++ PortfolioBlueJ/MenuUI.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class MenuUI here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MenuUI extends Main
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class MenuUI
+     */
+    public MenuUI()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
